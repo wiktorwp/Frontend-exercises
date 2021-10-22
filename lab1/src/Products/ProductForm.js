505ba1320eb64e102d97ae151990e1e988128962
Wiktor Płużek <w.pluzek.409@studms.ug.edu.pl>
2021-10-22 18:18:53 +0200

    lab1z3

--- lab1/src/Products/ProductForm.js
+++ lab1/src/Products/ProductForm.js
@@ -1,6 +1,16 @@
+// eslint-disable-next-line
 import React, {useState, useEffect} from 'react'
 import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+// eslint-disable-next-line
 const axios = require('axios');
+
+
+const productSchema = Yup.object().shape({
+    title: Yup.string().required('Tytuł jest wymagany'),
+    price: Yup.number().required('Cena jest wymagana'),
+    category: Yup.string().required('Kategoria jest wymagana'),
+})
 
 
 function ProductForm({ onSubmit, initialValues }) {
@@ -10,13 +20,17 @@ function ProductForm({ onSubmit, initialValues }) {
     return (
         <Formik
             initialValues={initialValues}
+            validationSchema={productSchema}
             onSubmit={onSubmit}
             enableReinitialize={true}
         >
             <Form>
-                <Field name="title"></Field>
-                <Field name="price" />
-                <Field name="category" />
+                <div>Tytuł <Field name="title"></Field></div>
+                <ErrorMessage name="title" component="div" />
+                <div>Cena <Field name="price" /></div>
+                <ErrorMessage name="price" component="div" />
+                <div>Kategoria <Field name="category" /></div>
+                <ErrorMessage name="category" component="div" />
                 <button type="submit">Zatwierdź</button>
             </Form>
 
--- lab1/src/Products/ProductList.js
+++ lab1/src/Products/ProductList.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from 'react'
+// eslint-disable-next-line
 import ProductForm from './ProductForm';
 import ProductCreate from './ProductCreate';
+// eslint-disable-next-line
+import { Formik, Field, Form, ErrorMessage} from "formik";
 const axios = require('axios')
 
 
 function ProductList() {
         const [products, setProducts] = useState([]);
 
+        // eslint-disable-next-line
 useEffect(async ()=>{
         await axios.get("https://fakestoreapi.com/products")
         .then(function (response) {
@@ -14,30 +18,26 @@ useEffect(async ()=>{
         })
 },[])
 
-const createProduct = async (newProduct) => {
+const createProduct = async (newProduct, {resetForm}) => {
+        resetForm({})
         const response = await axios.post("https://fakestoreapi.com/products", newProduct);
         console.log(response);
         setProducts([...products, response.data]);
     }
 
-//const createProduct = async (newProduct) => {
-//        const response = await axios.post("https://fakestoreapi.com/products", newProduct);
-//        console.log(response);
-//        setUsersList([...usersList, response.data]);
-//    }
+const deleteProduct = async (element) => {
+        const response = axios.delete(`https://fakestoreapi.com/products/${element.id}`);
+        console.log(response)
+        setProducts(products.filter(product => product.id !== element.id));
+}
 
-useEffect(()=>{
-        products.forEach(element => {
-                console.log(element)
-        });
-})
         return (
         
           <div className="List">
                   <ProductCreate onSubmit={createProduct}/>
             <div className="Products">
             {products.map(element=>
-            <div>ID:{element.id} <div>Nazwa: {element.title}</div> Cena: {element.price} Kategoria: {element.category}</div>)}
+            <div key={element.id}>ID:{element.id}<button onClick={()=>(deleteProduct(element))}>Delete</button> <div>Nazwa: {element.title}</div> Cena: {element.price} Kategoria: {element.category}</div>)}
             </div>
           </div>
         );
